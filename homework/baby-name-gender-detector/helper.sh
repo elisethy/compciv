4c9a4f694e4409855c7ca0eb0b7f87d00159dbb9
Elise Thygesen <eliset@corn36.stanford.edu>
2015-02-03 18:50:22 -0800

    My baby name gender detector!

---
+++ homework/baby-name-gender-detector/helper.sh
@@ -0,0 +1,5 @@
+mkdir data-hold
+cd ~/compciv/homework/baby-name-gender-detector/data-hold
+curl -o names.zip http://stash.compciv.org/ssa_baby_names/names.zip
+unzip -p names.zip yob1973.txt yob2013.txt | dos2unix > namesample.txt
+cd ~/compciv/homework/baby-name-gender-detector
