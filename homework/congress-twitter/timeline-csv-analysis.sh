015e0a8e3f2f9663688c74c662a5395567a46b3f
Elise Thygesen <eliset@corn29.stanford.edu>
2015-02-12 18:00:25 -0800

    my tweet analysis script

--- homework/congress-twitter/timeline-csv-analysis.sh
+++ homework/congress-twitter/timeline-csv-analysis.sh
@@ -9,8 +9,8 @@ echo "Analyzing $count tweets by $username since $lastdate"
 echo "Top 10 hashtags by $username"
 csvfix order -f 4 $file | grep -oE '#[[:alnum:]]+' | sort | uniq -c | sort -r | head -n 10
 echo "Top 10 retweeted users by $username"
-csvfix order -f 4 $file | grep -oE 'RT @[[:alnum:]]+' | grep -oE '@[[:alnum:]]+' | sort | uniq -c | sort -r | head -n 10
+csvfix order -f 4 $file | grep -oE 'RT @[[:alnum:]]+' | grep -oE '@[[:alnum:]]+' | tr '[[:upper:]]' '[[:lower:]]' | sort | uniq -c | sort -r | head -n 10
 echo "Top 10 mentioned users (not including retweets) by $username"
-csvfix order -f 4 $file | grep -v 'RT @[[:alnum:]]+' | grep -oE '@[[:alnum:]]+' | sort | uniq -c | sort -r | head -n 10
+csvfix order -f 4 $file | grep -v 'RT @[[:alnum:]]+' | grep -oE '@[[:alnum:]]+' | tr '[[:upper:]]' '[[:lower:]]' | sort | uniq -c | sort -r | head -n 10
 echo "Top 10 tweeted 10 words with 5+ letters by $username"
-csvfix order -f 4 $file | sed -E 's/@[[:alnum:]]+//g' | grep -oE '[[:alnum:]]{5,}' | sort | uniq -c | sort -r | head -n 10
+csvfix order -f 4 $file | sed -E 's/@[[:alnum:]]+//g' | grep -oE '[[:alnum:]]{5,}' | tr '[[:upper:]]' '[[:lower:]]' | sort | uniq -c | sort -r | head -n 10
