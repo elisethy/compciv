217f226506b9773613d2203d5040c972a50dbdd9
Elise Thygesen <eliset@corn29.stanford.edu>
2015-02-12 17:52:14 -0800

    my tweet analysis script!

--- homework/congress-twitter/timeline-csv-analysis.sh
+++ homework/congress-twitter/timeline-csv-analysis.sh
@@ -1,23 +1,10 @@
-# This assignment is just for readability purposes
 username=$1
-# create data-hold if it doesn't already exist
 mkdir -p ./data-hold
-echo "Fetching tweets for $username into ./data-hold/$username-timeline.csv
-"
-# use t to download the tweets in CSV form and save to file
+echo "Fetching tweets for $username into ./data-hold/$username-timeline.csv"
 file=data-hold/$username-timeline.csv
 t timeline -n 3200 --csv $username > $file
-# Get the count of lines using csvfix and its order subcommand
-# note: another subcommand could be used here, but the point is to use
-# csvfix to reduce the file to just the first field, and then count the lines.
-
-# In other words, you cannot just count the number of Tweets with wc alone, 
-# because some tweets span multiple lines
 count=$(csvfix order -f 1 $file | wc -l)
-# The timestamp of the tweet is in the field (i.e. column) named, 'Posted at'
-# and the oldest tweet is in the last line
 lastdate=$(csvfix order -fn 'Posted at' $file | tail -n 1)
-# Echoing some stats about the tweets
 echo "Analyzing $count tweets by $username since $lastdate"
 echo "Top 10 hashtags by $username"
 csvfix order -f 4 $file | grep -oE '#[[:alnum:]]+' | sort | uniq -c | sort -r | head -n 10
