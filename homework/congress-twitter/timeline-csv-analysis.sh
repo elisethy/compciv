0e1e3ac2d3f7cafa2ebbf8705563f9b124a77975
Elise Thygesen <eliset@corn13.stanford.edu>
2015-02-13 23:15:21 -0800

    my final tweet analysis!

--- homework/congress-twitter/timeline-csv-analysis.sh
+++ homework/congress-twitter/timeline-csv-analysis.sh
@@ -7,7 +7,7 @@ count=$(csvfix order -f 1 $file | wc -l)
 lastdate=$(csvfix order -fn 'Posted at' $file | tail -n 1)
 echo "Analyzing $count tweets by $username since $lastdate"
 echo "Top 10 hashtags by $username"
-csvfix order -f 4 $file | grep -oE '#[[:alnum:]]+' | sort | uniq -c | sort -r | head -n 10
+csvfix order -f 4 $file | grep -oE '#[[:alnum:]]+' | tr '[[:upper:]]' '[[:lower:]]' | sort | uniq -c | sort -r | head -n 10
 echo "Top 10 retweeted users by $username"
 csvfix order -f 4 $file | grep -oE 'RT @[[:alnum:]]+' | grep -oE '@[[:alnum:]]+' | tr '[[:upper:]]' '[[:lower:]]' | sort | uniq -c | sort -r | head -n 10
 echo "Top 10 mentioned users (not including retweets) by $username"
